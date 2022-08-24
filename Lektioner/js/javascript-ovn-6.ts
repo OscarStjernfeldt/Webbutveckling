52c3692ddc67490761028d36c18022f2dae2cd10
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-08-24 16:14:37 +0200

    Adds exercise 6.

---
+++ Lektioner/js/javascript-ovn-6.ts
@@ -0,0 +1,12 @@
+const form = document.querySelector("#form");
+const rain: HTMLInputElement = document.querySelector("#rain");
+const cold: HTMLInputElement = document.querySelector("#cold");
+const output = document.querySelector("#output");
+
+function showWeatherAndCold(event) {
+    event.preventDefault();
+
+    let rainingText = rain.checked ? "raining" : "not raining";
+    let coldText = cold.checked ? "cold" : "not cold";
+    output.textContent = `It is  ${rainingText} and it is  ${coldText}`;
+}
